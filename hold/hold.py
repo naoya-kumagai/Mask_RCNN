2bffbd326881320712b9deb9f032d5ab0e07e3b8
naoya <naoyakumagai1999@gmail.com>
2021-10-20 12:37:06 -0700

    hold.py

--- hold/hold.py
+++ hold/hold.py
@@ -1,28 +1,20 @@
 """
 Mask R-CNN
-Train on the toy Hold dataset and implement color splash effect.
-
+Train on the toy bottle dataset and implement color splash effect.
 Copyright (c) 2018 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
-
 ------------------------------------------------------------
-
 Usage: import the module (see Jupyter notebooks for examples), or run from
        the command line as such:
-
     # Train a new model starting from pre-trained COCO weights
-    python3 hold.py train --dataset=/path/to/hold/dataset --weights=coco
-
+    python3 hold.py train --dataset=path/to/dataset --weights=coco
     # Resume training a model that you had trained earlier
-    python3 hold.py train --dataset=/path/to/hold/dataset --weights=last
-
+    python3 hold.py train --dataset=/path/to/dataset --weights=last
     # Train a new model starting from ImageNet weights
-    python3 hold.py train --dataset=/path/to/hold/dataset --weights=imagenet
-
+    python3 hold.py train --dataset=/path/to/dataset --weights=imagenet
     # Apply color splash to an image
     python3 hold.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
-
     # Apply color splash to video using the last weights you trained
     python3 hold.py splash --weights=last --video=<URL or path to file>
 """
@@ -33,9 +25,12 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
+import cv2
+from mrcnn.visualize import display_instances
+import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.path.abspath("../")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -54,7 +49,7 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 ############################################################
 
 
-class HoldConfig(Config):
+class CustomConfig(Config):
     """Configuration for training on the toy  dataset.
     Derives from the base Config class and overrides some values.
     """
@@ -63,7 +58,7 @@ class HoldConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 1  # Background + hold
@@ -79,10 +74,10 @@ class HoldConfig(Config):
 #  Dataset
 ############################################################
 
-class HoldDataset(utils.Dataset):
+class CustomDataset(utils.Dataset):
 
-    def load_hold(self, dataset_dir, subset):
-        """Load a subset of the Hold dataset.
+    def load_custom(self, dataset_dir, subset):
+        """Load the hold dataset.
         dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
@@ -94,7 +89,7 @@ class HoldDataset(utils.Dataset):
         dataset_dir = os.path.join(dataset_dir, subset)
 
         # Load annotations
-        # VGG Image Annotator (up to version 1.6) saves each image in the form:
+        # VGG Image Annotator saves each image in the form:
         # { 'filename': '28503151_5b5b7ec140_b.jpg',
         #   'regions': {
         #       '0': {
@@ -108,9 +103,9 @@ class HoldDataset(utils.Dataset):
         #   'size': 100202
         # }
         # We mostly care about the x and y coordinates of each region
-        # Note: In VIA 2.0, regions was changed from a dict to a list.
-        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
-        annotations = list(annotations.values())  # don't need the dict keys
+        annotations1 = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+        # print(annotations1)
+        annotations = list(annotations1.values())  # don't need the dict keys
 
         # The VIA tool saves images in the JSON even if they don't have any
         # annotations. Skip unannotated images.
@@ -118,14 +113,11 @@ class HoldDataset(utils.Dataset):
 
         # Add images
         for a in annotations:
+            # print(a)
             # Get the x, y coordinaets of points of the polygons that make up
-            # the outline of each object instance. These are stores in the
+            # the outline of each object instance. There are stores in the
             # shape_attributes (see json format above)
-            # The if condition is needed to support VIA versions 1.x and 2.x.
-            if type(a['regions']) is dict:
-                polygons = [r['shape_attributes'] for r in a['regions'].values()]
-            else:
-                polygons = [r['shape_attributes'] for r in a['regions']] 
+            polygons = [r['shape_attributes'] for r in a['regions']]
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
@@ -135,23 +127,22 @@ class HoldDataset(utils.Dataset):
             height, width = image.shape[:2]
 
             self.add_image(
-                "hold",
+                "hold",  ## for a single class just add the name here
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
                 width=width, height=height,
                 polygons=polygons)
 
     def load_mask(self, image_id):
-        #https://github.com/matterport/Mask_RCNN/issues/2154
         """Generate instance masks for an image.
-        Returns:
+       Returns:
         masks: A bool array of shape [height, width, instance count] with
             one mask per instance.
         class_ids: a 1D array of class IDs of the instance masks.
         """
-        # If not a damage dataset image, delegate to parent class.
+        # If not a hold dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
-        if image_info["source"] != "damage":
+        if image_info["source"] != "hold":
             return super(self.__class__, self).load_mask(image_id)
 
         # Convert polygons to a bitmap mask of shape
@@ -160,21 +151,13 @@ class HoldDataset(utils.Dataset):
         mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
                         dtype=np.uint8)
         for i, p in enumerate(info["polygons"]):
-            if p['name'] == 'ellipse':
-                rr, cc = skimage.draw.ellipse(p['cy'], p['cx'], p['ry'], p['rx'])
-                mask[rr, cc, i] = 1
-
-            elif (p['name'] == 'polygon') or (p['name'] == 'polyline'):
-                rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
-                mask[rr, cc, i] = 1
-
-            else:
-                raise Exception('Unknown annotation type. Supported annotation types: Polygon, Polyline, Ellipse.')
+            # Get indexes of pixels inside the polygon and set them to 1
+            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            mask[rr, cc, i] = 1
 
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
         return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
-
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -188,13 +171,13 @@ class HoldDataset(utils.Dataset):
 def train(model):
     """Train the model."""
     # Training dataset.
-    dataset_train = HoldDataset()
-    dataset_train.load_hold(args.dataset, "train")
+    dataset_train = CustomDataset()
+    dataset_train.load_custom(args.dataset, "train")
     dataset_train.prepare()
 
     # Validation dataset
-    dataset_val = HoldDataset()
-    dataset_val.load_hold(args.dataset, "val")
+    dataset_val = CustomDataset()
+    dataset_val.load_custom(args.dataset, "val")
     dataset_val.prepare()
 
     # *** This training schedule is an example. Update to your needs ***
@@ -204,7 +187,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=10,
                 layers='heads')
 
 
@@ -212,19 +195,18 @@ def color_splash(image, mask):
     """Apply color splash effect.
     image: RGB image [height, width, 3]
     mask: instance segmentation mask [height, width, instance count]
-
     Returns result image.
     """
     # Make a grayscale copy of the image. The grayscale copy still
     # has 3 RGB channels, though.
     gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    # We're treating all instances as one, so collapse the mask into one layer
+    mask = (np.sum(mask, -1, keepdims=True) >= 1)
     # Copy color pixels from the original color image where mask is set
-    if mask.shape[-1] > 0:
-        # We're treating all instances as one, so collapse the mask into one layer
-        mask = (np.sum(mask, -1, keepdims=True) >= 1)
+    if mask.shape[0] > 0:
         splash = np.where(mask, image, gray).astype(np.uint8)
     else:
-        splash = gray.astype(np.uint8)
+        splash = gray
     return splash
 
 
@@ -279,7 +261,6 @@ def detect_and_color_splash(model, image_path=None, video_path=None):
         vwriter.release()
     print("Saved to ", file_name)
 
-
 ############################################################
 #  Training
 ############################################################
@@ -289,13 +270,13 @@ if __name__ == '__main__':
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect holds.')
+        description='Train Mask R-CNN to detect custom class.')
     parser.add_argument("command",
                         metavar="<command>",
                         help="'train' or 'splash'")
     parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/hold/dataset/",
-                        help='Directory of the Hold dataset')
+                        metavar="/path/to/custom/dataset/",
+                        help='Directory of the custom dataset')
     parser.add_argument('--weights', required=True,
                         metavar="/path/to/weights.h5",
                         help="Path to weights .h5 file or 'coco'")
@@ -324,9 +305,9 @@ if __name__ == '__main__':
 
     # Configurations
     if args.command == "train":
-        config = HoldConfig()
+        config = CustomConfig()
     else:
-        class InferenceConfig(HoldConfig):
+        class InferenceConfig(CustomConfig):
             # Set batch size to 1 since we'll be running inference on
             # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
             GPU_COUNT = 1
@@ -350,7 +331,7 @@ if __name__ == '__main__':
             utils.download_trained_weights(weights_path)
     elif args.weights.lower() == "last":
         # Find last trained weights
-        weights_path = model.find_last()
+        weights_path = model.find_last()[1]
     elif args.weights.lower() == "imagenet":
         # Start from ImageNet trained weights
         weights_path = model.get_imagenet_weights()
